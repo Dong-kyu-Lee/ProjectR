8116e58f7d095e402e7034a350b136eb978a61c0
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2024-12-01 12:09:44 +0900

    적 기본 상태
    적 상태 패턴 구현 시도
    - 상태 인터페이스 및 idle 상태 구현

---
+++ Assets/Scripts/IdleState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IState
+{
+    private Enemy enemy;
+
+    public IdleState(Enemy enemy)
+    {
+        this.enemy = enemy;
+    }
+
+    public void Enter()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+
+}
