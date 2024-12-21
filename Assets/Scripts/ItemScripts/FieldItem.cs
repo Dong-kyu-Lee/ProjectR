5347aab73a1cc12bfb8dcac9d7c24523afc4cff2
DualDura <jdsan216@gmail.com>
2024-12-21 17:59:23 +0900

    아이템 획득 로직 변경 #8
    # FieldItem 스크립트 : 플레이어가 E키를 누르면 소모품 아이템이 획득되게 변경

--- Assets/Scripts/ItemScripts/FieldItem.cs
+++ Assets/Scripts/ItemScripts/FieldItem.cs
@@ -10,17 +10,38 @@ public class FieldItem : MonoBehaviour
     private ConsumableItemData myItemData;  //필드에 떨어진 아이템의 정보
     [SerializeField]
     private int amount = 1;                 //습득했을 경우 얻게되는 아이템 수
+    private PlayerItemUseController playerController = null;    //플레이어의 아이템 사용 컨트롤러
 
     private void Awake()
     {
         GetComponent<SpriteRenderer>().sprite = myItemData.ItemSprite;
     }
 
+    private void Update()
+    {
+        if (playerController != null && Input.GetKeyDown(KeyCode.E))
+        {
+            AddToPlayerInventory(playerController);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            AddToPlayerInventory(collision.GetComponent<PlayerItemUseController>());
+            //아이템 설명 활성화
+            Debug.Log("아이템 설명 활성화");
+            playerController = collision.GetComponent<PlayerItemUseController>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //아이템 설명 비활성화
+            Debug.Log("아이템 설명 비활성화");
+            playerController = null;
         }
     }
 
