badb396d7325595b3b0826b0ec5b553bd212253c
DualDura <jdsan216@gmail.com>
2025-02-15 23:02:31 +0900

    만취, 출혈 수류탄 구현 #63
    + 만취 수류탄 프리펩 생성
    + 출혈 수류탄 프리펩 생성

--- Assets/Scripts/ItemScripts/TempDebuffArea.cs
+++ Assets/Scripts/ItemScripts/TempDebuffArea.cs
@@ -11,10 +11,14 @@ using UnityEngine;
 // 해당 스크립트는 로직의 수정이 좀 필요해보임.
 public class TempDebuffArea : MonoBehaviour
 {
+    [SerializeField]
     private Vector2 size = new Vector2(10, 0.5f);       //장판오브젝트의 크기
+    [SerializeField]
     private float maxTimeToDelete = 15.0f;              //임시생성된 장판이 사라지기까지의 시간
+    [SerializeField]
     private float maxTimeToDebuff = 10.0f;              //대상에게 디버프가 부여될때까지 걸리는 시간
     [SerializeField] private BuffType buffType;         //부여될 디버프 타입
+    
     private WaitForSeconds waitForZeroPointOneSec = new WaitForSeconds(0.1f);   //0.1초짜리 WaitForSeconds 객체
     private Dictionary<GameObject, float> debuffCounters = new Dictionary<GameObject, float>(); //해당 장판위에 있는 대상들의 디버프 카운터
     private Coroutine checkCounterCoroutine = null; //디버프 카운터들을 증감시키는 코루틴 변수
@@ -24,10 +28,9 @@ public class TempDebuffArea : MonoBehaviour
     {
         get { return size; }
         set 
-        { 
-            if (value.x < 0.0f) value.x = 0;
-            if (value.y < 0.0f) value.y = 0;
-            size = value;
+        {
+            size.x = value.x < 0.0f ? 0.0f : value.x;
+            size.y = value.y < 0.0f ? 0.0f : value.y;
         }
     }
 
