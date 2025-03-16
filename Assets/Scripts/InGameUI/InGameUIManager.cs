15d78605b7fcc7a276dacb05738e68935b0ad06e
Blu_E_Trash <eluci569@gmail.com>
2025-03-16 19:59:38 +0900

    기본적인 UI 배치
    메인 UI, 일시정지 UI 제작

--- Assets/Scripts/InGameUI/InGameUIManager.cs
+++ Assets/Scripts/InGameUI/InGameUIManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameUIManager : MonoBehaviour
 {
-    
-    void Start()
+    [SerializeField]
+    GameObject stopUI;
+    [SerializeField]
+    GameObject checkUI;
+    private bool isOpen;
+    private void Awake()
     {
-        
+        checkUI.SetActive(false);
+        stopUI.SetActive(false);
+        isOpen = false;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)&& isOpen == false)
+        {
+            stopUI.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape)&&isOpen == true)
+        {
+            stopUI.SetActive(false);
+        }
+    }
+    private void ContinueButton()
+    {
+        stopUI.SetActive(false);
+    }
+    private void ToLobby()
+    {
+        stopUI.SetActive(false);
+
+    }
+    private void ExitButton()
+    {
+        Application.Quit();
     }
 }
