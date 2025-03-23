d09b9522e2695f9ab9c18debf5ef5fbb9170a436
Blu_E_Trash <eluci569@gmail.com>
2025-03-23 16:19:16 +0900

    InGameUIManager싱글톤화
    InGameUIManager싱글톤화

--- Assets/Scripts/InGameUI/InGameUIManager.cs
+++ Assets/Scripts/InGameUI/InGameUIManager.cs
@@ -6,16 +6,42 @@ using UnityEngine.UI;
 
 public class InGameUIManager : MonoBehaviour
 {
+    private static InGameUIManager instance;
+    public static InGameUIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject InGameUIManagerObject = new GameObject("InGameUIManager");
+                instance = InGameUIManagerObject.AddComponent<InGameUIManager>();
+                DontDestroyOnLoad(InGameUIManagerObject);
+            }
+            return instance;
+        }
+    }
+
     [SerializeField]
     GameObject stopUI;
     [SerializeField]
     GameObject checkUI;
+    [SerializeField]
+    Text goldText;
+    PlayerStatus playerStatus;
     private bool isOpen;
+
     private void Awake()
     {
         checkUI.SetActive(false);
         stopUI.SetActive(false);
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        if (playerObject != null)
+        {
+            playerStatus = playerObject.GetComponent<PlayerStatus>();
+            Debug.Log("할당됨");
+        }
         isOpen = false;
+        goldText.text = playerStatus.Gold.ToString();
     }
     private void Update()
     {
