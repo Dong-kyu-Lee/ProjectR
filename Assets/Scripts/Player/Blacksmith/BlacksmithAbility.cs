dc2de5366ab58e1f8f9800d0245a9acb25e4b05a
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-04-27 18:25:01 +0900

    강화 확률 가중치 적용
    강화 단계 별 확률 적용
    - 0~9강 까지 강화 단계 적용
    - 각 강화 단계 별 확률 변동

--- Assets/Scripts/Player/Blacksmith/BlacksmithAbility.cs
+++ Assets/Scripts/Player/Blacksmith/BlacksmithAbility.cs
@@ -21,6 +21,11 @@ public class BlacksmithAbility : MonoBehaviour, IAbility
 
     public UnityEvent onAbilityUpdated;
 
+    // 강화 확률 리스트 (0강 ~ 9강)
+    List<float> enchantSuccessRates = new List<float> {
+        1.0f, 0.9f, 0.8f, 0.7f, 0.6f, 0.5f, 0.4f, 0.35f, 0.3f
+    };
+
     public void Activate()
     {
         if (!isActivated)
@@ -28,7 +33,7 @@ public class BlacksmithAbility : MonoBehaviour, IAbility
             isActivated = true;
             curWeaponData = weaponDataList[1];
             Debug.Log("CurWeapon : " + curWeaponData.WeaponName);
-            onAbilityUpdated.Invoke();   
+            onAbilityUpdated.Invoke();
         }
     }
 
@@ -42,32 +47,56 @@ public class BlacksmithAbility : MonoBehaviour, IAbility
 
     public void EnchantWeapon()
     {
-        float prob = Random.Range(0f, 1f);
-        if (prob > 0.5f)
+        if (enchantLevel >= enchantSuccessRates.Count)
         {
-            Debug.Log(prob);
-            Debug.Log("성공");
+            Debug.Log("최대 강화 단계");
+            return;
+        }
+
+        float prob = Random.Range(0f, 1f);
+        float successRate = GetSuccessRate(enchantLevel);
+
+        Debug.Log($"[강화 시도] 현재 강화 단계: {enchantLevel}, 성공 확률: {successRate * 100f}%, 뽑은 값: {prob}");
+
+        if (prob <= successRate)
+        {
+            Debug.Log("강화 성공");
             ++enchantLevel;
         }
+        else
+        {
+            Debug.Log("강화 실패");
+        }
+
         onAbilityUpdated.Invoke();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         Initialize();
         Debug.Log("CurWeapon : " + curWeaponData.WeaponName);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void Initialize()
     {
         isActivated = false;
         enchantLevel = 0;
+    }
+
+    float GetSuccessRate(int level)
+    {
+        if (level < enchantSuccessRates.Count)
+        {
+            return enchantSuccessRates[level];
+        }
+        else
+        {
+            return enchantSuccessRates[enchantSuccessRates.Count - 1];
+        }
     }
 }
