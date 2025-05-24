78aad2b87492382df8394950bb821a66ff126588
Dong-Kyu-Lee <stvboy00@gmail.com>
2025-05-24 17:19:10 +0900

    적 스폰 전 스폰 이팩트 구현

--- Assets/Scripts/DungeonScripts/EnemyInRoom.cs
+++ Assets/Scripts/DungeonScripts/EnemyInRoom.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 public class EnemyInRoom : MonoBehaviour
 {
     private int killCount;
+    public float enemySpawnDelay;
 
     // 생성할 적 프리펩
     public GameObject firstWaveEnemy;
     public GameObject secondWaveEnemy;
+    public GameObject EnemySpawnEffect;
 
     // 생성된 적 오브젝트
     public List<GameObject> firstWaveEnemyList = new List<GameObject>();
     public List<GameObject> secondWaveEnemyList = new List<GameObject>();
+    public List<GameObject> enemySpawnEffectList = new List<GameObject>();
 
     private void Start()
     {
@@ -27,8 +30,11 @@ public class EnemyInRoom : MonoBehaviour
             Destroy(firstWaveEnemyList[i]);
         for (int i = 0; i < secondWaveEnemyList.Count; ++i)
             Destroy(secondWaveEnemyList[i]);
+        for (int i = 0; i < enemySpawnEffectList.Count; ++i)
+            Destroy(enemySpawnEffectList[i]);
         firstWaveEnemyList.Clear();
         secondWaveEnemyList.Clear();
+        enemySpawnEffectList.Clear();
 
         var enemySpawnPositions = room.enemyTilemap.GetComponentsInChildren<Transform>();
         for(int i = 1; i < enemySpawnPositions.Length; ++i)
@@ -40,23 +46,45 @@ public class EnemyInRoom : MonoBehaviour
     // 적 오브젝트를 생성하고 비활성화하는 함수
     private void InitEnemySpawner(Vector3 generatePosition)
     {
+        // 첫번재 웨이브 적 인스턴스 생성
         if (firstWaveEnemy != null)
         {
             firstWaveEnemyList.Add(Instantiate(firstWaveEnemy, generatePosition, transform.rotation, transform));
             firstWaveEnemyList[firstWaveEnemyList.Count - 1].GetComponent<Enemy>().onDeath += EnemyDied;
             firstWaveEnemy.SetActive(false);
         }
+        // 두번째 웨이브 적 인스턴스 생성
         if (secondWaveEnemy != null)
         {
             secondWaveEnemyList.Add(Instantiate(secondWaveEnemy, generatePosition, transform.rotation, transform));
             secondWaveEnemyList[secondWaveEnemyList.Count - 1].GetComponent<Enemy>().onDeath += EnemyDied;
             secondWaveEnemy.SetActive(false);
         }
+        // 적 생성 이팩트 인스턴스 생성
+        enemySpawnEffectList.Add(Instantiate(EnemySpawnEffect, generatePosition, transform.rotation, transform));
+        enemySpawnEffectList[enemySpawnEffectList.Count - 1].SetActive(false);
     }
 
-    // 몬스터 웨이브를 생성하는 함수
+    // 적 웨이브를 생성하는 함수
     public void ActivateEnemies(bool isSecondWave)
     {
+        StartCoroutine(EnemySpawnCoroutine(isSecondWave));
+    }
+
+    // 적 생성 로직 코루틴
+    IEnumerator EnemySpawnCoroutine(bool isSecondWave)
+    {
+        // 이펙트 활성화
+        foreach (var item in enemySpawnEffectList)
+        {
+            item.SetActive(true);
+        }
+        yield return new WaitForSeconds(enemySpawnDelay);
+        foreach (var item in enemySpawnEffectList)
+        {
+            item.SetActive(false);
+        }
+        // 적 생성
         if (!isSecondWave)
         {
             foreach (var item in firstWaveEnemyList)
