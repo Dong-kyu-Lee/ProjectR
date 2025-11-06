e536a2509f6dcc22843fed09c35ead41648886d1
LeeDongGyu <stvboy00@gmail.com>
2025-11-07 00:41:12 +0900

    인게임 미션UI 구현완료

--- Assets/Scripts/InGameUI/MissionUI.cs
+++ Assets/Scripts/InGameUI/MissionUI.cs
@@ -12,6 +12,10 @@ public class MissionUI : MonoBehaviour
     public Button openButton;
     public Button closeButton;
 
+    [SerializeField] private Animator bgAnimator;
+    [SerializeField] private MissionUI_BG bgScript; // 배경 애니메이션 이벤트를 받기 위한 클래스
+    private bool isOpen = false;
+
     [Header("Background Image Size")]
     public float openPosX = -100;
     public float openHeight = 210;
@@ -19,14 +23,12 @@ public class MissionUI : MonoBehaviour
     public float closeHeight = 50;
 
 
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        openButton.gameObject.SetActive(true);
+        closeButton.gameObject.SetActive(false);
+        description.gameObject.SetActive(false);
+        killCountText.gameObject.SetActive(false);
     }
 
     public void SetMissionDescription(string desc)
@@ -42,21 +44,44 @@ public class MissionUI : MonoBehaviour
     // 미션 열기 화살표 버튼 클릭 시 호출
     public void OpenMissionUI()
     {
-        // background 이미지 길이를 부드럽게 늘리고 위치 조정
-
-        // open 버튼 비활성화
-
-        // close 버튼 활성화
-
-        // 미션 설명과 처치한 적 수 텍스트 활성화
+        // background On 애니메이션 실행
+        bgAnimator.SetBool("doMissionOn", true);
+        openButton.gameObject.SetActive(false);
+        // 애니메이션 종료 시 호출될 이벤트 함수 등록
+        bgScript.onAnimationEnd += OnAnimationEnd;
+        isOpen = true;
     }
 
     // 미션 닫기 화살표 버튼 클릭 시 호출
     public void CloseMissionUI()
     {
-        // background 이미지 길이를 부드럽게 줄이고 위치 조정
-        // close 버튼 비활성화
-        // open 버튼 활성화
+        // background Off 애니메이션 실행
+        bgAnimator.SetBool("doMissionOn", false);
+        closeButton.gameObject.SetActive(false);
+        // 애니메이션 종료 시 호출될 이벤트 함수 등록
+        bgScript.onAnimationEnd += OnAnimationEnd;
+        isOpen = false;
         // 미션 설명과 처치한 적 수 텍스트 비활성화
+        description.gameObject.SetActive(false);
+        killCountText.gameObject.SetActive(false);
+    }
+
+    // 애니메이션 종료 시 호출되는 이벤트 함수
+    public void OnAnimationEnd()
+    {
+        if(isOpen)
+        {
+            // close 버튼 활성화
+            closeButton.gameObject.SetActive(true);
+            // 미션 설명과 처치한 적 수 텍스트 활성화
+            description.gameObject.SetActive(true);
+            killCountText.gameObject.SetActive(true);
+        }
+        else
+        {
+            // open 버튼 활성화
+            openButton.gameObject.SetActive(true);
+            // 미션 설명과 처치한 적 수 텍스트 비활성화
+        }
     }
 }
