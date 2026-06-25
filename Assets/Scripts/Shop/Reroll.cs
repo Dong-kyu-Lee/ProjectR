e9a07e2784a6f4b64fcd14b4229dcc97624d1646
Blu_E_Trash <eluci569@gmail.com>
2026-06-25 17:24:08 +0900

    리롤 오브젝트의 소모 골드 양식 통일
    리롤 오브젝트의 소모 골드 양식 통일 및 플레이어 참조 방식 변경
    FindGameObjectWithTag -> PlayerManager.Instance.CurrentPlayer를 통해 참조

--- Assets/Scripts/Shop/Reroll.cs
+++ Assets/Scripts/Shop/Reroll.cs
@@ -2,75 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class Reroll : MonoBehaviour
 {
     [SerializeField]
-    ShopManager itemSlotManager;
+    private ShopManager itemSlotManager;
     [SerializeField]
-    TextMeshPro rerollCoastTxt;
-    private int rerollCoast;
-    private int rerollCount;
+    private TextMeshPro rerollCostTxt;
 
+    private int rerollCoast;
     public bool inRoll = false;
     private bool canReroll;
-    [SerializeField]
+
     private PlayerStatus playerStatus;
 
     private void Awake()
     {
-        if (playerStatus == null)
-            playerStatus = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerStatus>();
-
-        rerollCount = 0;
         rerollCoast = 50;
+        UpdateRerollText();
     }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && inRoll)
         {
-            if (rerollCoast <= playerStatus.Gold)
-                canReroll = true;
-            else
-                canReroll = false;
-            
-            if (canReroll && inRoll)
+            // 플레이어가 범위 내에 있을 때만 골드 체크 및 리롤 진행
+            if (playerStatus != null)
             {
-                RerollItem();
-            }
-            else if (!canReroll && inRoll)
-            {
-                Debug.Log("돈이 부족합니다.");
+                if (rerollCoast <= playerStatus.Gold)
+                    canReroll = true;
+                else
+                    canReroll = false;
+
+                if (canReroll)
+                {
+                    RerollItem();
+                }
+                else
+                {
+                    Debug.Log("돈이 부족합니다.");
+                }
             }
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             inRoll = true;
+
+            // PlayerManager 싱글톤을 이용하여 현재 플레이어의 PlayerStatus 컴포넌트를 가져옵니다.
+            if (PlayerManager.Instance.CurrentPlayer != null)
+            {
+                playerStatus = PlayerManager.Instance.CurrentPlayer.GetComponent<PlayerStatus>();
+            }
+            // 만약 PlayerManager에 할당이 안 되어있다면 트리거된 오브젝트에서 직접 가져오는 방어 코드
+            else
+            {
+                playerStatus = collision.GetComponent<PlayerStatus>();
+            }
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             inRoll = false;
+            playerStatus = null; // 범위를 벗어나면 참조를 해제하여 안전하게 관리합니다.
         }
     }
+
     private void RerollItem()
     {
         playerStatus.Gold -= rerollCoast;
         itemSlotManager.SellingItem();
-        rerollCount++;
-        if (rerollCoast != 200)
+
+        if (rerollCoast < 200)
         {
-            rerollCoastTxt.text = ((rerollCount + 1) * 50).ToString() + "G";
             rerollCoast += 50;
         }
-        else
-        {
-            rerollCoastTxt.text = "200G";
-            rerollCoast = 200;
-        }
+
+        UpdateRerollText();
+    }
+
+    private void UpdateRerollText()
+    {
+        rerollCostTxt.text = $"Reroll\n<color=yellow>{rerollCoast}G</color>";
     }
 }
