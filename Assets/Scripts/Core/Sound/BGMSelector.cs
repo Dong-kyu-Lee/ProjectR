55f6301950b07448b604f47cdb9fa77ec55c06ac
LeeDongGyu <stvboy00@gmail.com>
2026-09-16 14:40:30 +0900

    어떤 씬에서 어떤 BGM을 나오게 할 지 관리하는 클래스 생성 - LobbyScene에 BGM 안 나오는 버그 수정 중 변경중인 코드 - 기존에는 각 씬 관리 클래스에서 SoundManager를 호출해 원하는 BGM을 틀었다. - 하지만 LobbyScene에서는 씬 관리 클래스가 없어 다른 클래스가 사운드까지 관리하는 역할을 맡고(이에 따른 버그가 지금과 같이 발생), 씬이 추가될 때마다 해당 씬 코드에 사운드 코드를 추가해야 한다. - 이를 해결하기 위해 BGM 만 BGMSelector 클래스를 통해 모든 씬에 대해 통일되게 관리하도록 한다.

---
+++ Assets/Scripts/Core/Sound/BGMSelector.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+// 어떤 씬에서 어떤 BGM을 재생할지 결정하는 클래스
+// 예외적으로 프롤로그, 스토리 관련 씬은 해당 씬의 TimeLine이 BGM을 재생함.
+public class BGMSelector : MonoBehaviour
+{
+    [Serializable]
+    public struct BGMInfo
+    {
+        public string sceneName;
+        public AudioClip bgm;
+    }
+
+    [SerializeField] private List<BGMInfo> bgmList = new List<BGMInfo>();
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneChanged;
+    }
+
+    private void OnSceneChanged(Scene scene, LoadSceneMode mode)
+    {
+        // 현재 씬에 해당하는 BGM을 찾아 재생
+        foreach (var bgmInfo in bgmList)
+        {
+            if (bgmInfo.sceneName == scene.name)
+            {
+                SoundManager.Instance.Play(bgmInfo.bgm, Sound.Bgm);
+                return;
+            }
+        }
+    }
+}
